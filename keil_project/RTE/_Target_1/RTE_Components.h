841145f6fd6289b1013593ef0fc54c050f669e3c
Vik <ufo281@outlook.com>
2026-07-09 21:51:58 +0800

    [V1.0.0][add]{ add files }

---
+++ keil_project/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,54 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32g431_keil_pro' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g4xx.h"
+
+/* ARM.FreeRTOS::RTOS:Config:FreeRTOS:11.1.0 */
+#define RTE_RTOS_FreeRTOS_CONFIG        /* RTOS FreeRTOS Config for FreeRTOS API */
+/* ARM.FreeRTOS::RTOS:Core:Cortex-M:11.1.0 */
+#define RTE_RTOS_FreeRTOS_CORE          /* RTOS FreeRTOS Core */
+/* ARM.FreeRTOS::RTOS:Coroutines:11.1.0 */
+#define RTE_RTOS_FreeRTOS_COROUTINE     /* RTOS FreeRTOS Co-routines */
+/* ARM.FreeRTOS::RTOS:Event Groups:11.1.0 */
+#define RTE_RTOS_FreeRTOS_EVENTGROUPS   /* RTOS FreeRTOS Event Groups */
+/* ARM.FreeRTOS::RTOS:Heap:Heap_4:11.1.0 */
+#define RTE_RTOS_FreeRTOS_HEAP_4        /* RTOS FreeRTOS Heap 4 */
+/* ARM.FreeRTOS::RTOS:Message Buffer:11.1.0 */
+#define RTE_RTOS_FreeRTOS_MESSAGE_BUFFER /* RTOS FreeRTOS Message Buffers */
+/* ARM.FreeRTOS::RTOS:Stream Buffer:11.1.0 */
+#define RTE_RTOS_FreeRTOS_STREAM_BUFFER /* RTOS FreeRTOS Stream Buffers */
+/* ARM.FreeRTOS::RTOS:Timers:11.1.0 */
+#define RTE_RTOS_FreeRTOS_TIMERS        /* RTOS FreeRTOS Timers */
+/* ARM::CMSIS:RTOS2:FreeRTOS:Cortex-M:11.1.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_FreeRTOS        /* CMSIS-RTOS2 FreeRTOS */
+/* Keil::CMSIS Driver:SPI:1.1.0 */
+#define RTE_Drivers_SPI1                /* Driver SPI1 */
+        #define RTE_Drivers_SPI2                /* Driver SPI2 */
+        #define RTE_Drivers_SPI3                /* Driver SPI3 */
+        #define RTE_Drivers_SPI4                /* Driver SPI4 */
+/* Keil::CMSIS Driver:SPI:Multi-Slave:1.0.1 */
+#define RTE_Drivers_SPI_MultiSlave      /* Driver SPI Multi-Slave */
+/* Keil::CMSIS Driver:USART:1.0.0 */
+#define RTE_Drivers_USART1              /* Driver USART1 */
+        #define RTE_Drivers_USART2              /* Driver USART2 */
+        #define RTE_Drivers_USART3              /* Driver USART3 */
+        #define RTE_Drivers_USART4              /* Driver USART4 */
+        #define RTE_Drivers_USART5              /* Driver USART5 */
+        #define RTE_Drivers_USART6              /* Driver USART6 */
+
+
+#endif /* RTE_COMPONENTS_H */
